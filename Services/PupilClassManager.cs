ee552528564ba459c15d132f3cb60929dc578478
Tudor Iliescu <tudor.iliescu@gmail.com>
2025-06-08 22:41:07 +0300

    Added last refactorings.

--- Services/PupilClassManager.cs
+++ Services/PupilClassManager.cs
@@ -54,27 +54,42 @@ public class PupilClassManager
 
     public (List<UpdatedPupil>, List<UpdatedClass>) Diff(State oldState, State newState)
     {
+        List<UpdatedPupil> updatedPupils = GenerateUpdatedPupils(oldState, newState);
+        List<UpdatedClass> updatedClasses = GenerateUpdatedClasses(oldState, newState);
+
+        return (updatedPupils, updatedClasses);
+    }
+
+    private List<UpdatedClass> GenerateUpdatedClasses(State oldState, State newState)
+    {
+        var oldClasses = oldState.Classes;
+        var newClasses = newState.Classes;
+        var updatedClasses = new List<UpdatedClass>();
+
+        foreach (var item in oldClasses)
+        {
+
+            var shouldAddUpdatedClass = TryGetUpdatedClass(newClasses, item, out var updatedClass);
+
+            if (shouldAddUpdatedClass)
+            {
+                updatedClasses.Add(updatedClass);
+            }
+        }
+
+        return updatedClasses;
+    }
+
+    private List<UpdatedPupil> GenerateUpdatedPupils(State oldState, State newState)
+    {
         var oldPupils = oldState.Pupils;
         var newPupils = newState.Pupils;
         var updatedPupils = new List<UpdatedPupil>();
 
         foreach (var item in oldPupils)
         {
-            var shouldAddUpdatedPupil = false;
-            var updatedPupil = new UpdatedPupil();
-            var newPupil = newPupils.First(x => x.Id == item.Id);
-            if (newPupil.ClassName != item.ClassName)
-            {
-                shouldAddUpdatedPupil = true;
-                updatedPupil.PupilId = item.Id;
-                updatedPupil.ClassName = newPupil.ClassName;
-            }
 
-            if (newPupil.FollowUpNumber != item.FollowUpNumber)
-            {
-                shouldAddUpdatedPupil = true;
-                updatedPupil.FollowUpNumber = newPupil.FollowUpNumber;
-            }
+            var shouldAddUpdatedPupil = TryGetUpdatedPupil(newPupils, item, out var updatedPupil);
 
             if (shouldAddUpdatedPupil)
             {
@@ -82,28 +97,42 @@ public class PupilClassManager
             }
         }
 
-        var oldClasses = oldState.Classes;
-        var newClasses = newState.Classes;
-        var updatedClasses = new List<UpdatedClass>();
+        return updatedPupils;
+    }
 
-        foreach (var item in oldClasses)
+    private bool TryGetUpdatedClass(List<Class> newClasses, Class item, out UpdatedClass updatedClass)
+    {
+        var shouldAddUpdatedClass = false;
+        updatedClass = new UpdatedClass();
+        var newClass = newClasses.First(x => x.Id == item.Id);
+        if (newClass.AmountOfPupils != item.AmountOfPupils)
         {
-            var shouldAddUpdatedClass = false;
-            var updatedClass = new UpdatedClass();
-            var newClass = newClasses.First(x => x.Id == item.Id);
-            if (newClass.AmountOfPupils != item.AmountOfPupils)
-            {
-                shouldAddUpdatedClass = true;
-                updatedClass.ClassId = item.Id;
-                updatedClass.AmountOfPupils = newClass.AmountOfPupils;
-            }
-                        
-            if (shouldAddUpdatedClass)
-            {
-                updatedClasses.Add(updatedClass);
-            }
+            shouldAddUpdatedClass = true;
+            updatedClass.ClassId = item.Id;
+            updatedClass.AmountOfPupils = newClass.AmountOfPupils;
         }
 
-        return (updatedPupils, updatedClasses);
+        return shouldAddUpdatedClass;
+    }
+
+    private bool TryGetUpdatedPupil(List<Pupil> newPupils, Pupil item, out UpdatedPupil updatedPupil)
+    {
+        var shouldAddUpdatedPupil = false;
+        updatedPupil = new UpdatedPupil();
+        var newPupil = newPupils.First(x => x.Id == item.Id);
+        if (newPupil.ClassName != item.ClassName)
+        {
+            shouldAddUpdatedPupil = true;
+            updatedPupil.PupilId = item.Id;
+            updatedPupil.ClassName = newPupil.ClassName;
+        }
+
+        if (newPupil.FollowUpNumber != item.FollowUpNumber)
+        {
+            shouldAddUpdatedPupil = true;
+            updatedPupil.FollowUpNumber = newPupil.FollowUpNumber;
+        }
+
+        return shouldAddUpdatedPupil;
     }
 }
