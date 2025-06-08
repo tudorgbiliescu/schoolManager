f1e315243f9c808691c3aa90a1dd0139ad5ee0dd
Tudor Iliescu <tudor.iliescu@gmail.com>
2025-06-08 15:48:01 +0300

    Made first test pass by creating copy constructor for state, pupil and class objects.

--- Models/State.cs
+++ Models/State.cs
@@ -7,6 +7,16 @@ namespace SchoolManager.Models;
  */
 public class State
 {
+    public State()
+    {
+            
+    }
+    public State(State state)
+    {
+      Pupils = state.Pupils.Select(x => new Pupil(x)).ToList(); 
+      Classes = state.Classes.Select(x => new Class(x)).ToList();
+     
+    }
     public List<Pupil> Pupils { get; set; } = new();
 
     public List<Class> Classes { get; set; } = new();
--- Services/PupilClassManager.cs
+++ Services/PupilClassManager.cs
@@ -1,4 +1,5 @@
 using SchoolManager.Models;
+using SchoolManager.Models.Db;
 using SchoolManager.Models.Diff;
 
 namespace SchoolManager.Services;
@@ -12,11 +13,85 @@ public class PupilClassManager
 {
     public State UpdatePupilClassDivision(State state, Request request)
     {
-        throw new NotImplementedException("It's your job to implement this method.");
+        var newState = new State(state);
+        
+        foreach (var assignment in request.Assignments)
+        {
+            var pupilEntity = newState.Pupils.First(x => x.Id == assignment.PupilId);
+            var classEntity = newState.Classes.First(x => x.Id == assignment.ClassId);
+
+            if (classEntity.AmountOfPupils + 1 <= classEntity.MaxAmountOfPupils)
+            {
+                pupilEntity.ClassName = classEntity.ClassName;
+                classEntity.AmountOfPupils += 1;
+            }
+
+        }
+
+        foreach (var classItem in newState.Classes)
+        {
+            var classPupils = newState.Pupils.Where(x => x.ClassName == classItem.ClassName).OrderBy(x => x.Name).ToList();
+            for (int i = 1; i <= classPupils.Count(); i++)
+            {
+                var pupil = classPupils[i-1];
+                pupil.FollowUpNumber = i;
+            }
+        }
+        return newState;
     }
 
     public (List<UpdatedPupil>, List<UpdatedClass>) Diff(State oldState, State newState)
     {
-        throw new NotImplementedException("It's your job to implement this method.");
+        var oldPupils = oldState.Pupils;
+        var newPupils = newState.Pupils;
+        var updatedPupils = new List<UpdatedPupil>();
+
+        foreach (var item in oldPupils)
+        {
+            var shouldAddUpdatedPupil = false;
+            var updatedPupil = new UpdatedPupil();
+            var newPupil = newPupils.First(x => x.Id == item.Id);
+            if (newPupil.ClassName != item.ClassName)
+            {
+                shouldAddUpdatedPupil = true;
+                updatedPupil.PupilId = item.Id;
+                updatedPupil.ClassName = newPupil.ClassName;
+            }
+
+            if (newPupil.FollowUpNumber != item.FollowUpNumber)
+            {
+                shouldAddUpdatedPupil = true;
+                updatedPupil.FollowUpNumber = newPupil.FollowUpNumber;
+            }
+
+            if (shouldAddUpdatedPupil)
+            {
+                updatedPupils.Add(updatedPupil);
+            }
+        }
+
+        var oldClasses = oldState.Classes;
+        var newClasses = newState.Classes;
+        var updatedClasses = new List<UpdatedClass>();
+
+        foreach (var item in oldClasses)
+        {
+            var shouldAddUpdatedClass = false;
+            var updatedClass = new UpdatedClass();
+            var newClass = newClasses.First(x => x.Id == item.Id);
+            if (newClass.AmountOfPupils != item.AmountOfPupils)
+            {
+                shouldAddUpdatedClass = true;
+                updatedClass.ClassId = item.Id;
+                updatedClass.AmountOfPupils = newClass.AmountOfPupils;
+            }
+                        
+            if (shouldAddUpdatedClass)
+            {
+                updatedClasses.Add(updatedClass);
+            }
+        }
+
+        return (updatedPupils, updatedClasses);
     }
 }
